acf9d0e58431725f5a96d4c633938bcd9e3bb53d
yaomianxian <yaomianxian@vcredit.cn>
2024-07-31 15:12:53 +0800

    add brand demo

--- app/router/brand_router.py
+++ app/router/brand_router.py
@@ -51,3 +51,29 @@ async def brand():
         print(product_names)
     else:
         print("请求失败:", response_data["msg"])
+
+
+@router.get("/xxgk")
+async def xxgk():
+    for i in range(10, 101):
+        url = "https://www.szgm.gov.cn/xxgk/xqgwhxxgkml/gzgg/index_" + str(i) + ".html"
+        headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Cookie": "Path=/; bm_uuid=42022042; ft_szgov=0; arialoadData=false; laravel_session=eyJpdiI6IktcL09iQW1Fa3JQMWsyUDdFS0diRU1nPT0iLCJ2YWx1ZSI6InVRQW0xU2gxYTBRSitVUWh0UG93cWVxYlNkNEhWdk4yV0Y1M0FiOEs1VE9ZWHpuSEMxSHNMV1h5Q3IzOTQwNVAiLCJtYWMiOiJlMjkyZWU3ZjNhMjEwNThhZTQ5MzM1ZGQ1Y2JiYWVlYTUyNWZjN2EzNWNjNmJmNzYxNTdiMDEwOTA3ZjNhYWI1In0%3D; front_uc_session=eyJpdiI6IkRUaHJWNjEydHM4VmdFZFFxUmFuWUE9PSIsInZhbHVlIjoiMFlSc3dIeUM2NHJoWWZ6RkJHXC9FZldSZ0NqZDBwM0ZLMGJZeld4RkNxelBaWno4UWNKOFNoaTlIUVRwTlFrMU4iLCJtYWMiOiJkMjI1N2ExNzQzYzczZDBmMGY0ODNiYzdlNGNhMmJmYzgwNDIyNTJiNGZiMmNlZGJkMDhiY2YxMjI2YmUwZTdjIn0%3D",
+            "Pragma": "no-cache",
+            "Referer": "https://www.szgm.gov.cn/",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\""
+        }
+        response = requests.get(url, headers=headers, timeout=10, verify=False)
+        print(response)
